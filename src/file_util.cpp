51d7c19cbc5a93f25b23842658c3270e787b939c
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-02-03 14:01:37 +0100

    refactor: missing header, improve memory footprint

--- src/file_util.cpp
+++ src/file_util.cpp
@@ -1,6 +1,7 @@
 #include "mags/file_util.h"
 
 #include <fstream>
+#include <filesystem>
 #include <iostream>
 
 namespace mags::io {
@@ -12,28 +13,21 @@ Graph read_from_file(const std::string &path) {
     return {};
   }
 
-  std::vector<std::pair<NodeID, NodeID>> edges;
   int u, v;
   int max_node_id = -1;
 
-  // Line 1: Read all edges into memory
   while (fin >> u >> v) {
-    edges.emplace_back(u, v);
-    max_node_id = std::max({max_node_id, u, v});
+    if (u > max_node_id) max_node_id = u;
+    if (v > max_node_id) max_node_id = v;
   }
-  fin.close();
 
-  // Line 2: Initialize Graph with n nodes
-  const int n = max_node_id + 1;
-  Graph graph(n);
+  fin.clear();
+  fin.seekg(0);
 
-  // Line 3: Add edges to adjacency lists
-  // Note: This does NOT ensure undirectedness or clean data yet;
-  // that will be handled by your mags::preprocess::clean_graph.
-  for (const auto& [from, to] : edges) {
-    if (from >= 0 && from < n) {
-      graph.at(from).push_back(to);
-    }
+  Graph graph(max_node_id + 1);
+
+  while (fin >> u >> v) {
+    graph.at(u).push_back(v);
   }
 
   return graph;
